cf9f9689594a285b8e091de3c97e10642f02cbf3
mayank-msj-singh <mayank.msj.singh@gmail.com>
2025-08-11 00:46:53 +0530

    __init__.py change

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,8 +1,8 @@
-from base import (
+from .base import (
 auth_token_context
 )
 
-from accounts import (
+from .accounts import (
 apollo_view_account,
 apollo_update_account,
 apollo_create_account,
@@ -12,13 +12,13 @@ apollo_update_account_stage_bulk,
 apollo_update_account_owner_bulk
 )
 
-from calls import (
+from .calls import (
 apollo_update_call,
 apollo_search_calls,
 apollo_create_call_record
 )
 
-from contacts import (
+from .contacts import (
 apollo_create_contact,
 apollo_update_contact,
 apollo_search_contacts,
@@ -27,7 +27,7 @@ apollo_list_contact_stages,
 apollo_update_contact_owners
 )
 
-from deals import (
+from .deals import (
 apollo_update_deal,
 apollo_view_deal,
 apollo_create_deal,
@@ -35,12 +35,12 @@ apollo_list_all_deals,
 apollo_list_deal_stages
 )
 
-from enrichment import (
+from .enrichment import (
 apollo_organisation_enrichment,
 apollo_bulk_organisation_enrichment
 )
 
-from miscellaneous import (
+from .miscellaneous import (
 apollo_list_email_accounts,
 apollo_list_users,
 apollo_get_all_custom_fields,
@@ -48,12 +48,12 @@ apollo_get_all_lists_and_tags,
 apollo_view_api_usage_stats
 )
 
-from search import (
+from .search import (
 apollo_organization_job_postings,
 apollo_news_articles_search
 )
 
-from sequences import (
+from .sequences import (
 apollo_search_sequences,
 apollo_check_email_stats,
 apollo_search_outreach_emails,
@@ -61,7 +61,7 @@ apollo_update_contact_status_in_sequence,
 apollo_add_contacts_to_sequence
 )
 
-from tasks import (
+from .tasks import (
 apollo_create_tasks,
 apollo_search_tasks
 )
